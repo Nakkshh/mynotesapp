c08bfa7fe36d5875bcb5e1857539a1cbd5f6e919
Nakkshh <nakshtrajain25@gmail.com>
2025-03-31 00:15:49 +0530

    first commit

---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Trash from '../icons/Trash'
+import {db} from '../appwrite/databases.js'
+import { useContext } from 'react'
+import { NoteContext } from '../context/NoteContext.jsx'
+
+const DeleteButton = ({ noteId }) => {
+
+    const {setNotes} = useContext(NoteContext);
+    const handleDelete = async () => {
+        db.notes.delete(noteId);
+        setNotes((prevState) =>
+            prevState.filter((note) => note.$id !== noteId)
+        );
+    }; 
+  return (
+    <div onClick={handleDelete}>
+        <Trash/>
+    </div>
+  );
+}
+
+export default DeleteButton
